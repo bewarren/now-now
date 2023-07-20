fa244acf2ea1989cb9a8a44c18b23d1838e0b6c1
Benjamin Warren <benwarren5020@gmail.com>
2023-07-20 13:46:49 +0200

    Cool navigation bar

--- container/mainContainer.tsx
+++ container/mainContainer.tsx
@@ -14,6 +14,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import Test from "../components/Test";
 import { Session } from "@supabase/supabase-js";
 import Account from "../components/Account";
+import { Animated, Dimensions, View } from "react-native";
 
 // import ProfileContainer from "./profileContainer";
 
@@ -26,6 +27,8 @@ const peopleName = "Friends";
 const Tab = createBottomTabNavigator();
 
 const MainContainer = ({ session }: { session: Session }) => {
+  const tabOffsetValue = React.useRef(new Animated.Value(0)).current;
+
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -33,6 +36,12 @@ const MainContainer = ({ session }: { session: Session }) => {
         screenOptions={{
           headerShown: true,
           tabBarActiveTintColor: "#2ad14e",
+          tabBarShowLabel: true,
+          tabBarStyle: {
+            backgroundColor: "white",
+            position: "absolute",
+            height: 90,
+          },
         }}
       >
         <Tab.Screen
@@ -43,6 +52,14 @@ const MainContainer = ({ session }: { session: Session }) => {
               <FontAwesomeIcon icon={faWallet} size={size} color={color} />
             ),
           }}
+          listeners={({ navigation, route }) => ({
+            tabPress: (e) => {
+              Animated.spring(tabOffsetValue, {
+                toValue: 0,
+                useNativeDriver: true,
+              }).start();
+            },
+          })}
         >
           {(props) => <Account key={session.user.id} session={session} />}
         </Tab.Screen>
@@ -59,10 +76,78 @@ const MainContainer = ({ session }: { session: Session }) => {
               />
             ),
           }}
+          listeners={({ navigation, route }) => ({
+            tabPress: (e) => {
+              Animated.spring(tabOffsetValue, {
+                toValue: getWidth() * 1.1,
+                useNativeDriver: true,
+              }).start();
+            },
+          })}
         />
+        <Tab.Screen
+          name={peopleName}
+          component={Test}
+          options={{
+            headerShown: true,
+            tabBarIcon: ({ size, color }) => (
+              <FontAwesomeIcon
+                icon={faPeopleArrows}
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+          listeners={({ navigation, route }) => ({
+            tabPress: (e) => {
+              Animated.spring(tabOffsetValue, {
+                toValue: getWidth() * 2.3,
+                useNativeDriver: true,
+              }).start();
+            },
+          })}
+        />
+        <Tab.Screen
+          name={profileName}
+          options={{
+            headerShown: true,
+            tabBarIcon: ({ size, color }) => (
+              <FontAwesomeIcon icon={faUser} size={size} color={color} />
+            ),
+          }}
+          listeners={({ navigation, route }) => ({
+            tabPress: (e) => {
+              Animated.spring(tabOffsetValue, {
+                toValue: getWidth() * 3.5,
+                useNativeDriver: true,
+              }).start();
+            },
+          })}
+        >
+          {(props) => <Account key={session.user.id} session={session} />}
+        </Tab.Screen>
       </Tab.Navigator>
+      <Animated.View
+        style={{
+          width: getWidth() - 10,
+          height: 2,
+          backgroundColor: "#2ad14e",
+          position: "absolute",
+          bottom: 89,
+          left: 15,
+          transform: [{ translateX: tabOffsetValue }],
+        }}
+      ></Animated.View>
     </NavigationContainer>
   );
+};
+
+const getWidth = () => {
+  let width = Dimensions.get("window").width;
+
+  width = width - 60;
+
+  return width / 4;
 };
 
 export default MainContainer;
