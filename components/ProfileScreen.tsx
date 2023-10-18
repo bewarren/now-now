b7e42ac88ff0a91138adad05063807daaeecb61c
Benjamin Warren <benwarren5020@gmail.com>
2023-10-18 16:07:11 +0200

    moved handle function to send screen

--- components/ProfileScreen.tsx
+++ components/ProfileScreen.tsx
@@ -130,19 +130,6 @@ const ProfileScreen = ({ session }: { session: Session }) => {
     }
   };
 
-  const handlePress = useCallback(async () => {
-    // Checking if the link is supported for links with custom URL scheme.
-    const supported = await Linking.canOpenURL(snapScanLink);
-
-    if (supported) {
-      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
-      // by some browser in the mobile
-      await Linking.openURL(snapScanLink);
-    } else {
-      Alert.alert(`Don't know how to open this URL: ${snapScanLink}`);
-    }
-  }, [snapScanLink]);
-
   const handleEdit = () => {
     setEdit((prevState) => !prevState);
   };
