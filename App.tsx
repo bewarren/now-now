e3d109544bdb9bbb52f38a84942d5548af803988
Benjamin Warren <benwarren5020@gmail.com>
2023-10-20 15:23:52 +0200

    trying to find crash

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { supabase } from "./lib/supabase";
 import Auth from "./components/Auth";
 import Account from "./components/Account";
-import { View } from "react-native";
+import { Alert, View } from "react-native";
 import { Session } from "@supabase/supabase-js";
 import MainContainer from "./container/mainContainer";
 import LoginContainer from "./container/loginContainer";
@@ -12,9 +12,14 @@ export default function App() {
   const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
+    supabase.auth
+      .getSession()
+      .then(({ data: { session } }) => {
+        setSession(session);
+      })
+      .catch((err) => {
+        Alert.alert("error");
+      });
 
     supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
